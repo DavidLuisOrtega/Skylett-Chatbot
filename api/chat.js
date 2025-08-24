b84ea7bc18483c854305409be5486a9077f3d9c2
David Ortega <davidortega@Mac.fios-router.home>
2025-08-24 09:04:21 -0400

    Update API functions to use Vercel handler format

--- api/chat.js
+++ api/chat.js
@@ -4,7 +4,7 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
